668a7801b7d190082aa6060b45e68256ae4df058
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2021-10-12 20:32:07 +0530

    Snurja.com media

--- src/Media.js
+++ src/Media.js
@@ -11,7 +11,7 @@ const Media = () => {
     <img src="/images/5.jpeg" alt="img_media"/>
     <img src="/images/6.jpeg" alt="img_media"/>
     <img src="/images/7.jpeg" alt="img_media"/>
-    <img src="/images/8.jpg" alt="img_media"/>
+    <img src="/images/8.jpg" alt="img_media_img"/>
     <img src="/images/solar street light.jpeg" alt="img_media"/>
     <img src="/images/9.JPG" alt="img_media"/>
     <img src="/images/10.jpeg" alt="img_media"/>

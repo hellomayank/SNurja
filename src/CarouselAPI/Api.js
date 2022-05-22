a691de0d014a1a8a734ce7d306984f0d694fa512
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2022-05-22 15:12:59 +0530

    images change

--- src/CarouselAPI/Api.js
+++ src/CarouselAPI/Api.js
@@ -5,18 +5,11 @@ export const data = [
     {
         "imageUrl": "/projectphoto/2.jpg"
     },
-    {
-        "imageUrl": "/projectphoto/3.jpg"
-    },
+
     {
         "imageUrl": "/projectphoto/4.jpg"
     },
-    {
-        "imageUrl": "/projectphoto/5.jpg"
-    },
-    {
-        "imageUrl": "/projectphoto/6.jpg"
-    },
+
     {
         "imageUrl": "/projectphoto/7.jpg"
     }
--- src/RadiusInvertor.js
+++ src/RadiusInvertor.js
@@ -3,14 +3,14 @@ import './RadiusInvertor.css'
 export const RadiusInvertor = () => {
     return (
         <>
-          <div className="Distributor">
-              <img src="/images/radiusivertor.png" alt="InvertorLogo"></img>
-              <div className="Distributor_Details">
-                  <h2 className="Distributor_Details_h2">Authorised Distributor Radius Invertor</h2>
-                  <button className="Distributor_button"><a href="/invertor">ReadMore..</a></button>
-              </div>
-             
-          </div>  
+            <div className="Distributor">
+                <img src="/projectphoto/6.jpg" alt="InvertorLogo"></img>
+                <div className="Distributor_Details">
+                    <h2 className="Distributor_Details_h2">Authorised Distributor Radius Invertor</h2>
+                    <button className="Distributor_button"><a href="/invertor">ReadMore..</a></button>
+                </div>
+
+            </div>
         </>
     )
 }
